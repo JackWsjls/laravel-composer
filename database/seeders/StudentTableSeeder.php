9d9da7d35f46091f2b3ce4ea88d74d49ddc7ab7c
wsjls <418383219@qq.com>
2022-06-23 10:20:17 +0800

    update:待完成文件上传

---
+++ database/seeders/StudentTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StudentTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('students')->insert([
+            ['name'=>'xiaoming', 'age'=>'18'],
+            ['name'=>'xiaofang', 'age'=>'17'],
+        ]);
+    }
+}
